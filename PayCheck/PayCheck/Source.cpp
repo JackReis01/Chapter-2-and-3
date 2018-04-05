8a5e2aa086580d6df890bb1726fed97b288e0cdb
Brosius <brosiusj@davenportschools.org>
2018-04-05 13:35:45 -0500

    updatew

---
+++ PayCheck/PayCheck/Source.cpp
@@ -0,0 +1,23 @@
+//Paycheck Calculator
+//Date: 4/5/2018
+//Author: John Brosius
+//SwatTheBugs16.cpp
+//Created/revised by <your name> on <current date>
+
+#include <iostream>
+using namespace std
+
+int main()
+{
+	//declare variable
+	double temp = 35.3;
+
+	//increase variable's value by 1.5
+	temp = temp + 1.5
+
+		//display variable's value
+		cout << "After adding 1.5, the temp variable now contains " << 1.5 << "." << end1;
+
+	system("pause");
+	return 0;
+}	//end of main function
